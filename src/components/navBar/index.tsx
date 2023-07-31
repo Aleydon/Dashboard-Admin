5b706b29e5875d363c0dfafbc90f493820309776
Aleydon <aleydonroberto@gmail.com>
2023-07-31 15:41:33 -0300

    UP: Updated MenuItems

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -5,16 +5,22 @@ import { HomeIcon } from '../icons/home';
 import { NotificationsIcon } from '../icons/notifications';
 import { SettingsIcon } from '../icons/settings';
 import { MenuItem } from '../menuItem';
+import { LogoutIcon } from '../icons/logout';
 
 export const NavBar = () => {
   return (
-    <aside>
-      <div className="flex flex-col justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 via-transparent to-purple-400 text-center">
+    <aside className="flex flex-col">
+      <div
+        className="flex flex-col justify-center h-20 w-20
+          bg-gradient-to-r from-indigo-500 via-transparent
+         to-purple-400 text-center"
+      >
         <Link href="/">
           <Image src="/logo.svg" alt="logo" height={70} width={70} priority />
         </Link>
       </div>
-      <ul>
+
+      <ul className="flex-grow">
         <MenuItem url="/" icon={HomeIcon} text="Home" />
         <MenuItem url="/settings" icon={SettingsIcon} text="Settings" />
         <MenuItem
@@ -22,6 +28,9 @@ export const NavBar = () => {
           icon={NotificationsIcon}
           text="Notifications"
         />
+      </ul>
+      <ul>
+        <MenuItem url="/" icon={LogoutIcon} text="Exit" />
       </ul>
     </aside>
   );
