be695c2a62ad5ac05196168f074f92fca687562e
Aleydon <aleydonroberto@gmail.com>
2023-07-31 10:35:10 -0300

    UP: Updated navBar component

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,5 +1,20 @@
-export const NavBar = () => (
-  <>
-    <h1>NavBar</h1>
-  </>
-);
+import { HomeIcon } from '../icons/home';
+import { NotificationsIcon } from '../icons/notifications';
+import { SettingsIcon } from '../icons/settings';
+import { MenuItem } from '../menuItem';
+
+export const NavBar = () => {
+  return (
+    <aside>
+      <ul>
+        <MenuItem url="/" icon={HomeIcon} text="Home" />
+        <MenuItem url="/settings" icon={SettingsIcon} text="Settings" />
+        <MenuItem
+          url="/notifications"
+          icon={NotificationsIcon}
+          text="Notifications"
+        />
+      </ul>
+    </aside>
+  );
+};
