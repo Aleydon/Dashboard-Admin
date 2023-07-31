af4c17b9eb257c9e9dab8feb79480b4e387d1625
Aleydon <aleydonroberto@gmail.com>
2023-07-31 14:32:15 -0300

    UP: Updated Layout  style

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -11,7 +11,7 @@ interface LayoutProps {
 
 export const Layout = ({ title, subTitle, children }: LayoutProps) => {
   return (
-    <div className="flex flex-col bg-gray-400 w-full ">
+    <div className="flex flex-col bg-gray-400 h-screen  w-screen ">
       <Header title={title} subTitle={subTitle} />
       <Content>{children}</Content>
     </div>
