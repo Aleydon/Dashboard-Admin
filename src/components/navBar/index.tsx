bb3765361d4af006898b0d1cfc81d44b18a2fc12
Aleydon <aleydonroberto@gmail.com>
2023-07-31 15:22:32 -0300

    UP: Added Logo Image

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { HomeIcon } from '../icons/home';
 import { NotificationsIcon } from '../icons/notifications';
 import { SettingsIcon } from '../icons/settings';
@@ -6,6 +9,11 @@ import { MenuItem } from '../menuItem';
 export const NavBar = () => {
   return (
     <aside>
+      <div className="flex flex-col justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 via-transparent to-purple-400 text-center">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" height={70} width={70} priority />
+        </Link>
+      </div>
       <ul>
         <MenuItem url="/" icon={HomeIcon} text="Home" />
         <MenuItem url="/settings" icon={SettingsIcon} text="Settings" />
