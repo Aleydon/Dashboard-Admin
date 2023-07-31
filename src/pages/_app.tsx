b4f616185e4122fc99563601db56691ec3483c1b
Aleydon <aleydonroberto@gmail.com>
2023-07-31 14:06:17 -0300

    ADD: NavBar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { NavBar } from '@/components/navBar';
+
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex h-screen w-screen">
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
